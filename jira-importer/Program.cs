b2b87ca9ef90e1e41c6b0c7a600a430b979af32c
Jason Best <skeeterbug@gmail.com>
2015-04-16 16:20:27 -0700

    Initial commit

---
+++ jira-importer/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
+using Newtonsoft.Json;
+
+namespace jira_importer
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var reader = XmlReader.Create("MBL.xml");
+			
+			var s = new XmlSerializer(typeof(YouTrackIssues));
+			YouTrackIssues issues = (YouTrackIssues)s.Deserialize(reader);
+
+			var jiraIssue = new JiraIssue();
+			jiraIssue.JiraProject.Key = "INFORCRM";
+			jiraIssue.Description = "foo";
+			jiraIssue.Summary = "bar";
+			jiraIssue.DefectId = "MBL-108049";
+			jiraIssue.JiraIssueType = JiraIssueType.Bug;
+
+			string json = JsonConvert.SerializeObject(jiraIssue, Newtonsoft.Json.Formatting.Indented);
+
+			Console.WriteLine(json);
+
+			Console.WriteLine();
+			Console.ReadLine();
+		}
+	}
+}
